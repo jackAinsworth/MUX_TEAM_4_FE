552b498fe478d22221c04eda85b75a1a23378608
Loic Tsayem <melachioloic99@yahoo.com>
2024-06-17 17:30:35 +0200

    profil end

--- myHealthApp/src/app/pages/profil/profil.page.ts
+++ myHealthApp/src/app/pages/profil/profil.page.ts
@@ -13,8 +13,18 @@ export class ProfilPage implements OnInit {
   notificationsEnabled: boolean = true;
   nightModeEnabled: boolean = false;
   managerViewEnabled: boolean = false;
+  heights: number[] = [];
+  stepLengths: number[] = [];
 
-  constructor() { }
+  constructor() {
+    // Génère une liste de tailles de 150 cm à 200 cm
+    for (let i = 150; i <= 200; i++) {
+      this.heights.push(i);
+    }
+    for (let y = 10; y <= 150; y++) {
+      this.stepLengths.push(y);
+    }
+  }
 
   ngOnInit() {
   }
