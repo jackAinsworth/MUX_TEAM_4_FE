01191552c0d9ab4d0ebc1aa156951f3daa7dc885
Loic Tsayem <melachioloic99@yahoo.com>
2024-06-19 20:42:33 +0200

    modal-rangliste-manager

--- myHealthApp/src/app/pages/rangliste-manager/rangliste-manager.page.ts
+++ myHealthApp/src/app/pages/rangliste-manager/rangliste-manager.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ModalController } from '@ionic/angular';
+import { StepsModalComponent } from '../../steps-modal/steps-modal.component';
+
 @Component({
   selector: 'app-rangliste-manager',
   templateUrl: './rangliste-manager.page.html',
@@ -8,7 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class RanglisteManagerPage implements OnInit {
 
   selectedItem: number = 3;
-  constructor() { }
+  constructor(private modalController: ModalController) { }
+
+  async openStepsModal() {
+    const modal = await this.modalController.create({
+      component: StepsModalComponent,
+      cssClass: 'alert-modal'
+    });
+    return await modal.present();
+  }
 
   onItemClick(itemNumber: number) {
     this.selectedItem = itemNumber; // Met à jour l'élément sélectionné
